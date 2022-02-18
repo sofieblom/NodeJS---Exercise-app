8ef6f8e29902fbe181937709e083f94327685ea6
Sofie Blom <sofie@MacBook-Pro-som-tillhor-Sofie.local>
2022-02-18 14:56:12 +0100

    created folders and files for the start structure and express-handlebars

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const port = 8000;
 
+app.engine('hbs', exphbs.engine({
+    extname: 'hbs', 
+    defaultLayout: 'main'
+}))
+
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({extended: true}))
+
+
 app.get('/', (req,res) => {
-    res.send('hello')
+    res.render('home')
 })
 
 app.listen(port, () => {
