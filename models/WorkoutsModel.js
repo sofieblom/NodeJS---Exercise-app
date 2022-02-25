6d1b8fd4b3d802b6158f9deeb36dbd27e5156c0d
sofieblom <ssofieblom@hotmail.com>
2022-02-25 14:25:56 +0100

    created workoutSchema and collection

---
+++ models/WorkoutsModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const workoutsSchema = new mongoose.Schema({
+    exerciseId: {string:String}, 
+    date: {type: Number, default: Date.now}
+})
+
+const ExercisesModel = mongoose.model('Workouts', workoutsSchema);
+
+module.exports = ExercisesModel;
