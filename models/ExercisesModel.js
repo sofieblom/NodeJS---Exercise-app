436b62837fcca11de086a940533b3cbae316640b
sofieblom <ssofieblom@hotmail.com>
2022-03-04 17:29:50 +0100

    added exercise property with ref:workouts

--- models/ExercisesModel.js
+++ models/ExercisesModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const exercisesSchema = new mongoose.Schema({
     title: {type: String, required: true}, 
     hardness: {type: Number, required: true},
-    description: {type: String, required: true}
+    description: {type: String, required: true},
+    workout: { type: mongoose.Schema.Types.ObjectId,  ref: 'Workouts'}
 });
 
 const ExercisesModel = mongoose.model('Exercises', exercisesSchema);
